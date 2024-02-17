1a1a0cc24b1130ced0c187649d322db421271571
Houssem Eddine Ammari <hea.s.engineer@gmail.com>
2024-02-17 11:39:10 +0100

    creating a dummy ProcessLine method

--- src/main/java/com/racemusconsulting/aircraftenvironmentallimitationsbackend/services/DataLoadingFromCsvService.java
+++ src/main/java/com/racemusconsulting/aircraftenvironmentallimitationsbackend/services/DataLoadingFromCsvService.java
@@ -1,11 +1,14 @@
 package com.racemusconsulting.aircraftenvironmentallimitationsbackend.services;
 
 import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.racemusconsulting.aircraftenvironmentallimitationsbackend.entities.AircraftModelEntity;
 import com.racemusconsulting.aircraftenvironmentallimitationsbackend.repositories.AircraftModelRepository;
 import com.racemusconsulting.aircraftenvironmentallimitationsbackend.repositories.TemperatureDeviationRepository;
 
@@ -28,6 +31,24 @@ public class DataLoadingFromCsvService {
 	}
 
 	private void processLine(String line) {
+		String[] values = line.split(";");
+		String model = values[0].trim();
+		AircraftModelEntity aircraftModel = findOrCreateAircraftModel(model);
+
+		if (values.length == 5) {
+			processAndSaveDeviations(aircraftModel, values[1], "MAX", "IN_FLIGHT");
+			processAndSaveDeviations(aircraftModel, values[2], "MIN", "IN_FLIGHT");
+			processAndSaveDeviations(aircraftModel, values[3], "MAX", "TOLD");
+			processAndSaveDeviations(aircraftModel, values[4], "MIN", "TOLD");
+		}
+	}
+	
+	private AircraftModelEntity findOrCreateAircraftModel(String model) {
+		return null;
+	}
+	
+	private void processAndSaveDeviations(AircraftModelEntity aircraftModel, String deviationData, String type,
+			String phase) {
 
 	}
 }
